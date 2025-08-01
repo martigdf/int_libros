6dd7dc04d082cda08acb957958bdd299a3aea154
martigdf <martina.guzman@correo.ucu.edu.uy>
2025-08-01 20:53:27 -0300

    siguiente paso ya funciona manifiest y service worker

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,14 +9,18 @@ import { tokenInterceptor } from './app/interceptors/token.interceptor';
 
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { environment } from './environments/environment.prod';
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient(withFetch(), withInterceptors([ tokenInterceptor ]))
+    provideHttpClient(withFetch(), withInterceptors([ tokenInterceptor ])), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ],
 });
 
